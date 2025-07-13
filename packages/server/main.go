13bccf0742d64ffd6ba10d5af7ff0eb3b5955757
Julien Rousseau <julrousseau20@gmail.com>
2025-07-12 21:38:43 -0400

    revoking

--- packages/server/main.go
+++ packages/server/main.go
@@ -20,7 +20,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	ctx := context.Background()
 	config := &oauth2.Config{
 		ClientID:     os.Getenv("OAUTH_CLIENT_ID"),
 		ClientSecret: os.Getenv("OAUTH_CLIENT_SECRET"),
@@ -32,8 +31,8 @@ func main() {
 		RedirectURL: "http://localhost:8888/oauth2/callback",
 	}
 
-	var verifierId uint16 = 0
-	var verifiers = make(map[uint16]string)
+	var verifierId uint8 = 0
+	var verifiers = make(map[uint8]string)
 
 	http.HandleFunc("GET /oauth2/authenticate", func(w http.ResponseWriter, r *http.Request) {
 		verifiers[verifierId] = oauth2.GenerateVerifier()
@@ -47,10 +46,11 @@ func main() {
 
 		verifierIdStr := q.Get("state")
 		vId, _ := strconv.Atoi(verifierIdStr)
-		v := verifiers[uint16(vId)]
-		delete(verifiers, uint16(vId))
+		v := verifiers[uint8(vId)]
+		delete(verifiers, uint8(vId))
 
 		code := q.Get("code")
+		ctx := context.Background()
 		token, err := config.Exchange(ctx, code, oauth2.VerifierOption(v))
 		if err != nil {
 			http.Error(w, "could not authenticate", http.StatusUnauthorized)
@@ -63,6 +63,25 @@ func main() {
 		fmt.Println(w, string(b))
 
 		http.Redirect(w, r, "http://localhost:5173", http.StatusTemporaryRedirect)
+	})
+	http.HandleFunc("POST /oauth2/revoke", func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+
+		if token[:len("Bearer ")] == "Bearer " {
+			token = token[len("Bearer "):]
+			url := fmt.Sprintf("https://oauth2.googleapis.com/revoke?token=%s", token)
+			r, _ := http.NewRequest("POST", url, nil)
+
+			c := http.Client{}
+			res, err := c.Do(r)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			defer res.Body.Close()
+		}
+
+		w.WriteHeader(http.StatusOK)
 	})
 
 	http.Handle("/*", http.RedirectHandler("/", http.StatusTemporaryRedirect))
