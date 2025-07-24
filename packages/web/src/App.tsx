fa48422227d536ed4ac1932d671c254148081f61
Julien Rousseau <julrousseau20@gmail.com>
2025-07-24 17:58:15 -0400

    updated app server url

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   function disconnect() {
-    fetch("http://localhost:8888/oauth2/revoke", {
+    fetch(`${import.meta.env.VITE_SERVER_URL}/oauth2/revoke`, {
       method: "post",
       credentials: "include",
     });
@@ -9,7 +9,10 @@ function App() {
   return (
     <>
       <h1>Blingpot</h1>
-      <a className="block" href="http://localhost:8888/oauth2/authenticate">
+      <a
+        className="block"
+        href={`${import.meta.env.VITE_SERVER_URL}/oauth2/authenticate`}
+      >
         authenticate
       </a>
       <button onClick={disconnect}>disconnect</button>
