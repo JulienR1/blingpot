91e74ab283ce20e64550d8844348b5312495b2df
Julien Rousseau <julrousseau20@gmail.com>
2025-07-27 14:40:15 -0400

    wip

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,17 +1,47 @@
-function App() {
-  function disconnect() {
-    fetch("/oauth2/revoke", { method: "post", credentials: "include" });
-  }
+import {
+  QueryClientProvider,
+  QueryClient,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { useCallback } from "react";
 
+const queryClient = new QueryClient();
+
+function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <h1>Blingpot</h1>
       <a className="block" href="/oauth2/authenticate">
         authenticate
       </a>
-      <button onClick={disconnect}>disconnect</button>
-    </>
+      <Disconnect />
+      <Profile />
+    </QueryClientProvider>
   );
+}
+
+function Disconnect() {
+  const q = useQueryClient();
+
+  const disconnect = useCallback(async () => {
+    await fetch("/oauth2/revoke", { method: "post", credentials: "include" });
+    q.invalidateQueries({ queryKey: ["user-profile"] });
+  }, [q]);
+
+  return <button onClick={disconnect}>disconnect</button>;
+}
+
+function Profile() {
+  const profile = useQuery({
+    queryKey: ["user-profile"],
+    queryFn: () =>
+      fetch("/profiles/me", { credentials: "include" })
+        .then((res) => res.text())
+        .catch(() => null),
+  });
+
+  return <pre>{JSON.stringify(profile.data, null, 2)}</pre>;
 }
 
 export default App;
